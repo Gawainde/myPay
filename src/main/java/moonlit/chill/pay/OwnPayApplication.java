d92fb248691098735f25bdea641d6d6da2a515f7
朔夜时雨 <14303759+MoonlitChill@user.noreply.gitee.com>
2024-11-05 15:51:50 +0800

    11

---
+++ src/main/java/moonlit/chill/pay/OwnPayApplication.java
@@ -0,0 +1,20 @@
+package moonlit.chill.pay;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Slf4j
+@EnableAsync
+@EnableScheduling
+@SpringBootApplication
+public class OwnPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnPayApplication.class, args);
+        log.info("启动成功");
+    }
+
+}
